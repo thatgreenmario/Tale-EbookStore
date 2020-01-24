efea9e7f6ec0f95908c00436b18174bb073c8fb2
priti.chougule5@gmail.com <priti.chougule5@gmail.com>
2020-01-24 20:28:26 +0530

    Cart Functionality working  and BookService created

--- Tale-ebook/src/app/cart/cart.component.ts
+++ Tale-ebook/src/app/cart/cart.component.ts
@@ -10,11 +10,23 @@ import { BookService } from '../book.service';
 export class CartComponent implements OnInit {
 
   constructor() { }
+  cartTotal =0;
+  booksInCart : Book[]= BookService.getCartBooks();
 
   ngOnInit() {
+      var price=0;
+      for(let i=0; i<this.booksInCart.length; i++){
+        price+=this.booksInCart[i].price;
+        this.cartTotal= price;
+    }
+
+  }
+  
+  onRemoveFromCart(event,book:Book)
+  {
+     // Item to remove
+this.booksInCart = this.booksInCart.filter(obj => obj !== book);
   }
 
-   cartTotal=0; 
-  booksInCart : Book[]= BookService.getCartBooks();
 }
 
