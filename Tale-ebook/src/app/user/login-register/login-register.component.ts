24c115439f7166d57ccd3b1625275dcd92dad204
subhendu <subhendupccoe@gmail.com>
2020-01-29 00:48:17 +0530

    auth service

--- Tale-ebook/src/app/user/login-register/login-register.component.ts
+++ Tale-ebook/src/app/user/login-register/login-register.component.ts
@@ -11,6 +11,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LoginRegisterComponent implements OnInit {
 
+  login_email: string;
+  login_password: string;
+
+  fname: string;
+  lname: string;
+  reg_email: string;
+  reg_password: string;
+
  
   constructor(private _elementRef:ElementRef,private router: Router, private authService: AuthService,private http:HttpClient) { }
 
@@ -25,32 +33,45 @@ export class LoginRegisterComponent implements OnInit {
   
   onLogin(form: NgForm) {
     
-    if (form.invalid) {
-      return;
-    }
-    
-    if(this.authService.login(form.value.email, form.value.password)==false)
-    alert( "Username or Password is Incorrect Please Try Again!");
-    
-    else
-    this.router.navigate(['/store']);
+    var user = {
+      "email": this.login_email,
+      "password": this.login_password
+    };
 
+    this.authService.login(user).then(
+      r => {
+        if (r == true)
+          this.router.navigate(['/store']);
+        else
+          alert("Username or Password is Incorrect Please Try Again!");
+      }
+    ).catch(e => {
+      alert('error fetching data');
+    });
   }
 
 
 
   onRegister(form: NgForm) {
-    if (form.invalid) {
-      return;
-    }
-    if(this.authService.createUser(form.value.fname, form.value.lname, form.value.email, form.value.password)==false)
-      form.resetForm();
-    else 
-     return;
-  }
+    var user = {
+      "firstname": this.fname,
+      "lastname": this.lname,
+      "email": this.reg_email,
+      "password": this.reg_password
 
+    };
+
+    this.authService.createUser(user).then(
+      r => {
+        if (r == true)
+          alert("Hi, " + user.firstname + ". Thankyou for registering. Please login to continue.")
+        else
+          alert("Hi, " + user.firstname + ". It seems you have already registered. Please login to continue, or click on forgot password to recover your password");
+      }
+    ).catch(e => {
+      alert('error fetching data');
+    });
 
 }
 
-
-
+}
